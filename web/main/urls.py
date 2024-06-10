ba899f95d1dbd2c72e8f940932e66e68fc683fea
Ben Davies <bendavies@he3.com.au>
2024-06-10 21:11:23 +1000

    Basic layout for website

--- web/main/urls.py
+++ web/main/urls.py
@@ -2,9 +2,9 @@
 from django.urls import path
 
 from . import views
-from .views import index
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("warehouses/", views.warehouse_list, name="warehouse_list"),
+    path("sitemap.json", views.sitemap, name="sitemap"),
+    path("", views.index, name="index"),
+    path("settings/warehouses/", views.warehouse_list, name="warehouse_list"),
 ]
--- web/main/views.py
+++ web/main/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 # Third Party
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # First Party
@@ -11,7 +11,7 @@ from .models import Warehouse
 
 
 def index(request):
-    return HttpResponse(get_test_message())
+    return render(request, "index.html", {})
 
 
 def warehouse_list(request):
@@ -19,3 +19,36 @@ def warehouse_list(request):
     return render(
         request, "settings/warehouse_list.html", {"warehouses": warehouses}
     )
+
+
+from django.http import JsonResponse
+
+
+def sitemap(request):
+    sitemap_data = {
+        "sections": {
+            "Projects": {
+                "Overview": "/projects/overview",
+                "Portfolio": "/projects/portfolio",
+                "Browse": "/projects/browse",
+            },
+            "Production": {"Overview": "/production/overview"},
+            "Compositions": {
+                "Batches": "/compositions/batches",
+                "Search": "/compositions/search",
+            },
+            "Lab": {
+                "Image Comparator": "/lab/image-comparator",
+                "Factory Settings": "/lab/factory-settings",
+                "Replacement Parts": "/lab/replacement-parts",
+            },
+            "Process": {
+                "Pipelines": "/process/pipelines",
+                "Factory Processes": "/process/factory-processes",
+                "Replacement Parts": "/process/replacement-parts",
+                "Warehouses": "/process/warehouses/",
+            },
+            "Settings": {"Warehouses": "/settings/warehouses/"},
+        }
+    }
+    return JsonResponse(sitemap_data)
